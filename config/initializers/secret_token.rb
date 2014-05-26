4e6032f35dea8723ebaa1dbb3544a3fedaf0408e
Daniele Palombo <ksemuldie@gmail.com>
2014-05-26 22:54:18 +0200

    init repository and rails 4 #12587927033447

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Genmon::Application.config.secret_key_base = '7c61792d5ff4427930e1ef4a2b71706ae70e4915739282c7b7657e1518508501c6f25cf63053bc70bbe445b6c3a17b6f8b9122efb6ff6dc8c864e8e06798a30f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Genmon::Application.config.session_store :cookie_store, key: '_genmon_session'
