abc90d04b90217e39f853b915b90fd602d1795f3
Daniele Palombo <ksemuldie@gmail.com>
2014-06-02 01:14:28 +0200

    powerunit test

--- app/models/power_unit.rb
+++ app/models/power_unit.rb
@@ -1,4 +1,8 @@
 class PowerUnit
   include Mongoid::Document
   field :code, type: String
+
+  validates :code, 
+    :presence => true , 
+    :uniqueness => {:case_sensitive => false}
 end
--- spec/models/power_unit_spec.rb
+++ spec/models/power_unit_spec.rb
@@ -2,5 +2,5 @@ require "spec_helper"
 
 describe PowerUnit do
   it { should validate_presence_of(:code) }
-  it { should validate_uniqueness_of(:code) }
+  it { should validate_uniqueness_of(:code).case_insensitive }
 end
