03b09b5ec428bab5ccfd3f188482404211aef5ac
Daniele Palombo <ksemuldie@gmail.com>
2014-06-18 00:00:49 +0200

    end state

--- app/models/concerns/stringable.rb
+++ app/models/concerns/stringable.rb
@@ -5,14 +5,6 @@ module Stringable
 
   included do
     field :state, type: String
-
-    #define_state_values({
-    #  0 => 'no alarm',
-    #  1 => 'pump no power',
-    #  2 => 'no fuel',
-    #  3 => 'pump timeout',
-    #  4 => 'pump on fail'
-    #})
   end
 
   module ClassMethods
